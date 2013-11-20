587711f20fb64b20b736119772f3474fcd2f99e4
Zachary Drudi <zachdrudi@gmail.com>
2013-11-20 12:07:45 -0800

    Proposals extend ordered to reduce boilerplate

--- proposal.scala
+++ proposal.scala
@@ -1,32 +1,14 @@
 package paxos
 
-class Proposal(p: Int, proposer_name : String) {
+class Proposal(p: Int, proposer_name : String) extends Ordered[Proposal] {
   val propnum = p
   val proposer = proposer_name
 
   var value = "blank"
 
-  def <(that: Proposal): Boolean = {
-    return (this.propnum < that.propnum || 
-      (this.propnum == that.propnum && this.proposer < that.proposer))
-  }
-
-  def >(that: Proposal) : Boolean = {
-    return that < this
-  }
-
-  def >=(that: Proposal) : Boolean = {
-    return that <= this
-  }
-
-  def <=(that: Proposal): Boolean = {
-    return (this.propnum < that.propnum || 
-      (this.propnum == that.propnum && this.proposer <= that.proposer))
-  }
-
-  def ==(that: Proposal) : Boolean = {
-    return (this.propnum == that.propnum && this.proposer == that.proposer && this.value == that.value)
-  }
+  def compare(that : Proposal) = 
+    if (this.propnum == that.propnum) this.proposer.compare(that.proposer)
+    else this.propnum.compare(that.propnum)
 
   def setValue(v : String) = {
     value = v
